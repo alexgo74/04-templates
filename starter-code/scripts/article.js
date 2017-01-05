d14ccc21da13f75cb15de45efdc6fb8278088cbc
Alejandro Garcia <agash2013@outlook.com>
2017-01-05 14:36:02 -0800

    added script to html file

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,6 +15,12 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
+  var source = $('#blog-template').html();
+  var templateRender = Handlebars.compile(source);
+  var context = rawData;
+  return templateRender(this);
+
+
 
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
